8206f4bb82fa87c70dc2af81a2336461af35273b
pedrovisk <pedro.h.marcondes.rocha@gmail.com>
2025-05-16 15:21:55 -0300

    versão web do sistema

---
+++ SistemaDePersonagens/DAOeINTERFACE/VERSÃOWEB/SistemaPersonagensWeb/src/java/controller/ControlePersonagens.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import DAO.PersonagensDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.Personagens;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Pedrovisk
+ */
+@WebServlet(name = "ControlePersonagens", urlPatterns = {"/ControlePersonagens"})
+public class ControlePersonagens extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+        String op = request.getParameter("op");
+        PersonagensDAO pdao = new PersonagensDAO();
+        Personagens p = new Personagens();
+        String mensagem = "";
+        if(op.equals("Cadastrar")) {
+                String nome = request.getParameter("txtnome");
+                String descricao = request.getParameter("txtdescricao");
+                double avaliacao = Double.parseDouble(request.getParameter("txtavaliacao"));
+                p.setNome(nome);
+                p.setDescricao(descricao);
+                p.setAvaliacao(avaliacao);
+                try {
+                    pdao.cadastrar(p);
+                    mensagem = "Cadastrado com sucesso";
+                } catch (ClassNotFoundException | SQLException ex) {
+                    mensagem = "ERRO" + ex.getMessage();
+            }
+        }
+
+        else if (op.equals("Deletar")) {
+            mensagem = "Não implementado";
+        }
+
+        else if (op.equals("Atualizar")) {
+            mensagem = "Não implementado";
+            }
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControlePersonagens</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Resultado: " + mensagem + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
