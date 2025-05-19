2507ff3385c39ebd4c999bce3bed080d0c24b4ec
pedrovisk <pedro.h.marcondes.rocha@gmail.com>
2025-05-19 11:45:36 -0300

    versão web atualizada com funções

---
+++ SistemaDePersonagens/SistemaPersonagensWeb/src/java/model/Personagens.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Pedrovisk
+ */
+public class Personagens {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private double avaliacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+}
